28ee3131ec8702230d4c3b722fa35471b254fce0
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-01 18:19:11 +0100

    created header and model description

--- Model/app.py
+++ Model/app.py
@@ -3,3 +3,9 @@ import torch
 import wikipedia as wk
 import transformers as tf
 import streamlit as st
+
+# Main application engine
+if __name__ == '__main__':
+    # Display name and titile
+    st.title("WikiMindAI - Wikipedia-based Mindful Artificial Intelligence")
+    st.write("Search topics, Ask questions, Get answers!")
