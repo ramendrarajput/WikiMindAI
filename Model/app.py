1c78dd0f2de96b5ca8a2ef8215838c708c071cff
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-08-03 00:14:27 +0100

    defined query and summary function

--- Model/app.py
+++ Model/app.py
@@ -4,6 +4,12 @@ import wikipedia as wk
 import transformers as tf
 import streamlit as st
 
+def load_wiki(query: str) -> str:
+    results = wk.search(query)
+    summary = wk.summary(results[0], sentences = 10)
+    return summary
+    
+
 # Main application engine
 if __name__ == '__main__':
     # Display name and titile
@@ -13,5 +19,3 @@ if __name__ == '__main__':
     # Topic Input
     topic = st.text_input("Search Topic", "")
     
-    # Question Input
-    question = st.text_input("Question", "")
