9ea6eef0043690afe9c40284c58adc25950387be
Gideon-Ogunbanjo <dejiogunbanjo9@gmail.com>
2023-12-17 19:39:52 +0100

    fixed bugs

--- Model/app.py
+++ Model/app.py
@@ -7,8 +7,6 @@ from pydub import AudioSegment
 from io import BytesIO
 import speech_recognition as sr
 import requests
-import torch
-import tensorflow as tf
 
 # Page configuration
 st.set_page_config(
